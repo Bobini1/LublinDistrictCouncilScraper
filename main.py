91a345649a3cb15a1b6d9ec3bad35b8d96260bc8
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2025-11-28 00:39:28 +0100

    Initialize calendar before collecting events

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from upload_events import Calendar
 
 
 def main():
+    calendar = Calendar("razem.lubelskie@gmail.com")
     # Get all future events
     events = get_all_future_events()
-
     # Upload events to the calendar
-    calendar = Calendar("ef0984b0b800fa348809b14eacf2a0b274a8aaf1954794a39f84ffb93e04952c@group.calendar.google.com")
     for event in events:
         calendar.send_event(event)
 
